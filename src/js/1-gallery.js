42c31bd66efe433a0d061cb7be9a58b9556534cc
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-02-26 13:12:05 +0200

    attempt-1

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,9 +1,26 @@
-
 import SimpleLightbox from "simplelightbox";
-
 import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
 
+const gallery = document.querySelector('.gallery');
 
-const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+const galleryCardMarkup = galleryItems
+  .map( item =>
+    `<li class="gallery-item">
+	<a class="gallery-link" href="${item.original} ">
+		<img 
+		    class="gallery-image" 
+			src="${item.preview}" 
+			alt="${item.description}" 
+		/>
+	</a>
+    </li>`)
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryCardMarkup);
 
-console.log(SimpleLightbox);
+let lightbox = new SimpleLightbox('.gallery li', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
+lightbox.on('shown.simplelightbox');
+
